93adb8c5b1b96609ad47c9afbe88f8e4431bf8db
NickW <nickwalker818323818@gmail.com>
2020-06-04 17:23:30 -0700

    Daily Report Tech Academy

---
+++ Daily Report Tech Academy/Daily Report Tech Academy/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Daily_Report_Tech_Academy
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("The Tech Academy");
+            Console.WriteLine("Student Daily Report");
+            Console.WriteLine("Please enter your name: ");
+            string studentName = Console.ReadLine();
+            Console.WriteLine("What course are you on?");
+            string courseName = Console.ReadLine();
+            Console.WriteLine("Do you need help with anything? Please answer \"true\" or \"false\".");
+            string assistanceNeeded = Console.ReadLine();
+            Console.WriteLine("Were there any positive experiences you'd like to share? Please give specifics.");
+            string specifics = Console.ReadLine();
+            Console.WriteLine("How many hours did you study today?");
+            string studyHours = Console.ReadLine();
+            Console.WriteLine("Thank you for your answers. An instructor will respond to this shortly. Have a great day!");
+
+        }
+    }
+}
