a21732626bc31072827af972e872c6db0478d21b
NickW <nickwalker818323818@gmail.com>
2020-06-06 11:06:42 -0700

    Boolean/Age

---
+++ Boolean/Boolean/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boolean
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("What is your age? ");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Have you ever had a DUI? ");
+            Console.WriteLine("Please answer true or false");
+            bool dui = Convert.ToBoolean(Console.ReadLine());
+            Console.WriteLine(dui);
+            Console.WriteLine("How many speeding tickets do you have? ");
+            int speedingTickets = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Qualified? ");
+            Console.WriteLine(age > 15 && dui == false && speedingTickets < 3);
+            Console.ReadLine();
+        }
+    }
+}
