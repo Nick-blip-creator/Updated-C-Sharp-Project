dadda6fbba56be6ab3bf456652ed07bda55c0710
NickW <nickwalker818323818@gmail.com>
2020-06-06 16:50:47 -0700

    Do While Loop

---
+++ Boolean Comparison/Boolean Comparison/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boolean_Comparison
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("How old am I? ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            bool myAge = number == 26;
+
+            do
+            {
+                switch (number)
+                {
+                    case 23:
+                        Console.WriteLine("How old am I? ");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 26:
+                        Console.WriteLine("You guess right! ");
+                        myAge = true;
+                        break;
+                    default:
+                        Console.WriteLine("How old am I? ");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+
+                }
+            }
+
+            while (!myAge);
+            Console.Read();
+        }
+    }
+}
+            
+                
+            
+        
+
+
