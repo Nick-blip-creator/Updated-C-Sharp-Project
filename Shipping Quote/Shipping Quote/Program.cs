1c1b0a0cf08334e8564f03369783d75506055ce5
NickW <nickwalker818323818@gmail.com>
2020-06-09 14:20:47 -0700

    CORRECTED STEP 117

--- Shipping Quote/Shipping Quote/Program.cs
+++ Shipping Quote/Shipping Quote/Program.cs
@@ -13,6 +13,13 @@ namespace Shipping_Quote
             Console.WriteLine("Welcome to Package Express. Please follow the instructions below ");
             Console.WriteLine("Please enter the package weight: ");
             int packageWeight = Convert.ToInt32(Console.ReadLine());
+            if (packageWeight > 50)
+            {
+                Console.WriteLine("Package too heavy to be shipped via Package Express");
+                Console.ReadLine();
+                Environment.Exit(0);
+            }
+
             Console.WriteLine("Please enter the package width: ");
             int packageWidth = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Please enter the package height: ");
@@ -20,13 +27,18 @@ namespace Shipping_Quote
             Console.WriteLine("Please enter the package length");
             int packageLength = Convert.ToInt32(Console.ReadLine());
 
-            int dimension = packageHeight * packageLength * packageWidth;
+            int dimension = packageHeight + packageLength + packageWidth;
 
-            if (packageWeight > 50)
+            if (dimension > 50)
             {
-                Console.WriteLine("Package too big to be shipped via Package Express");
+                Console.WriteLine("Package too big to be shipped via Package Express.");
+                Console.ReadLine();
+                Environment.Exit(0);
             }
-            int estimatedCost = dimension * packageWeight / 100;
+
+            
+            int estimatedCost = packageHeight * packageLength * packageWidth * packageWeight / 100;
+ 
             Console.WriteLine("Your estimateed total for shipping this package is: $" + estimatedCost) ;
             Console.WriteLine("Thank you");
 
