95366aa87ee755608b34d28e2f1888504895640e
NickW <nickwalker818323818@gmail.com>
2020-06-09 13:11:23 -0700

    Loop section

---
+++ ArraysAndLists/ArraysAndLists/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArraysAndLists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Select an index of the array ");
+            string[] city = { "Los Angeles", "London", "Portland", "Miami" };
+            int input = int.Parse(Console.ReadLine());
+            if (input > city.Length)
+            {
+                Console.WriteLine("This doesn't exist ");
+            }
+            Console.WriteLine("The city is : " + city[input]);
+
+            Console.WriteLine("Select an index for a number ");
+            int[] numbers = {1, 2, 3, 4, 5, 6 };
+            int numberImput = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("The number is : " + numbers[numberImput]);
+
+            
+            Console.Read();
+
+
+        }
+    }
+}
---
+++ Iterations/Iterations/Program.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iterations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please input something: ");
+            string carInput = Console.ReadLine();
+            List<string> cars = new List<string>() { "BMW", "Ferrari", "Bentley", "Rolls Royce" };
+
+            //Part1 
+            foreach (string car in cars)
+            {
+                Console.WriteLine(car + " " + carInput);
+            }
+            //Part 2 & Part3 fixed infinite loop
+            int i = 0;
+            while (!true)
+            {
+                Console.WriteLine(i);
+                i++;
+
+            }
+
+            //Part 4
+
+            int number = 500;
+            for (int x = number; x < 502; x++)
+            {
+                Console.WriteLine(x);
+            }
+
+            //Part 5
+            for (int x = 0; x <= 5; x++)
+            {
+                Console.WriteLine(x);
+            }
+
+            //Part 6 7 8
+
+            List<string> cities = new List<string>() { "Los Angeles", "London", "Portland", "Detroit" };
+            Console.WriteLine("Enter a city to see if it appears: ");
+            string cityInput = Console.ReadLine();
+            bool isThere = false;
+
+            foreach (string city in cities)
+            {
+                if (cityInput == city)
+                {
+                    Console.WriteLine("The city is here! ");
+                    int index1 = cities.IndexOf(city);
+                    Console.WriteLine(index1);
+                    isThere = true;
+                    break;
+                }
+            }
+            if (!isThere)
+            {
+                Console.WriteLine("City not found.");
+            }
+
+            //Part 9
+            List<string> streets = new List<string>() { "Hollywood Blvd", "Vineland", "Vineland", "Apple St" };
+            Console.WriteLine("Enter a street to see if it appears: ");
+            string streetInput = Console.ReadLine();
+            bool isTherel = false;
+
+
+
+            foreach (string street in streets)
+            {
+                if (streetInput == street)
+                {
+                    Console.WriteLine("The city is here! ");
+                    int index1 = streets.IndexOf(street);
+                    Console.WriteLine(index1);
+                    isTherel = true;
+                    break;
+                }
+            }
+            if (!isTherel)
+            {
+                Console.WriteLine("City not found.");
+            }
+            Console.WriteLine("S duplicate countries. ");
+            List<string> countries = new List<string>() { "Germany", "Germany", "Sweden", "Switzerland", "America" };
+
+            Console.WriteLine("Initial list of countries ...");
+            
+            foreach (string val1 in countries)
+            {
+                foreach (string val2 in countries)
+                {
+                    if (val1 == val2)
+                    {
+                        Console.WriteLine(val1 + " is a duplicate");
+                        break;
+                       
+                    }
+                
+                }
+                Console.Read();
+
+            }
+        }
+    }
+}
+
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
