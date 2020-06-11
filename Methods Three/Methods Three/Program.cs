cf124f7145c8f853b0f28ba5f67e030a7b8650e9
NickW <nickwalker818323818@gmail.com>
2020-06-10 20:35:30 -0700

    Step 161

---
+++ Methods Three/Methods Three/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods_Three
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            division divisionObject = new division();
+            Console.WriteLine(divisionObject.addition(5));
+            Console.WriteLine(divisionObject.subtraction(50));
+            Console.WriteLine(divisionObject.multipication(50));
+            Console.Read();
+
+
+        }
+    }
+}
---
+++ Methods Three/Methods Three/division.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods_Three
+{
+    public class division
+    {
+        public int addition(int add)
+        {
+            int total = add + 10;
+            return total;
+        }
+        public int subtraction(int subtract)
+        {
+            Console.WriteLine("Pick a number to subtract by 50 ");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+            int subtractionTotal = userInput - subtract;
+            return subtractionTotal;
+        }
+        public int multipication(int multiply)
+        {
+            Console.WriteLine("Pick a number to multiply by 50 ");
+            int usersInput = Convert.ToInt32(Console.ReadLine());
+            int multiplicationTotal = usersInput * multiply;
+            return multiplicationTotal;
+        }
+    }
+}
