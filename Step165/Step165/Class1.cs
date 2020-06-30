ecc9bba80cd2431a1acd0e74359200a8061f54a0
NickW <nickwalker818323818@gmail.com>
2020-06-30 11:39:18 -0700

    Step 165

---
+++ Step165/Step165/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step165
+{
+    class Class1
+    {
+        public int addition(int add1, [Optional] int add2)
+        {
+            int total = add1 + add2;
+            return total;
+        }
+    }
+}
---
+++ Step165/Step165/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step165
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 class1Object = new Class1();
+            Console.WriteLine(class1Object.addition(5));
+            Console.WriteLine(class1Object.addition(5, 38));
+            Console.Read();
+
+
+        }
+    }
+}
