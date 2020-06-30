b25a7ae8f91a93f93e8192ca083e1edb21ff1472
NickW <nickwalker818323818@gmail.com>
2020-06-30 15:53:23 -0700

    Levi, this is the final corrected one.

---
+++ CORRECTEdStep164/CORRECTEdStep164/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CORRECTEdStep164
+{
+    class Class1
+    {
+        public int addition(int add)
+        {
+            int total = add + 10;
+            return total;
+        }
+        public int addition(decimal add)
+        {
+            decimal fullTotal = add + 10.9m;
+            int result = Convert.ToInt32(fullTotal);
+            return result;
+        }
+        public int addition(string add)
+        {
+            int x = Convert.ToInt32(add);
+            x = x + 5;
+            return x;
+        }
+    }
+}
---
+++ CORRECTEdStep164/CORRECTEdStep164/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CORRECTEdStep164
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 class1Object = new Class1();
+            Console.Write(class1Object.addition(20));
+            Console.Write(class1Object.addition(20.82m));
+            Console.Write(class1Object.addition("10"));
+
+            Console.Read();
+        }
+    }
+}
