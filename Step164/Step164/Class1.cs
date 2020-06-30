ff5256c9eb287f164bfd809386ffc3faca701837
NickW <nickwalker818323818@gmail.com>
2020-06-30 11:09:53 -0700

    Step164

---
+++ Step164/Step164/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step164
+{
+    class Class1
+    {
+        public int addition(int add)
+            {
+            int total = add + 10;
+            return total;
+            }
+        public double addition(double add)
+        {
+            double fullTotal = add + 10.9;
+            return fullTotal;
+        }
+        public string addition(string add)
+        {
+            string spellNumber = add + "Hello";
+            return spellNumber;
+        }
+    }
+}
---
+++ Step164/Step164/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step164
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 class1Object = new Class1();
+            Console.WriteLine(class1Object.addition(20));
+            Console.WriteLine(class1Object.addition(20.82));
+            Console.WriteLine(class1Object.addition(31.72 + "Hi"));
+
+
+            Console.Read();
+        }
+    }
+}
