954cfb8d50c2e6bc4e59052ad7193833dbeb1a88
NickW <nickwalker818323818@gmail.com>
2020-07-01 12:33:36 -0700

    Step 174

---
+++ Step174/Step174/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step174
+{
+    class Employee : Person
+    {
+        public int ID { get; set; }
+
+    }
+}
---
+++ Step174/Step174/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step174
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employed = new Employee();
+            employed.FirstName = " Sample ";
+            employed.LastName = "Student ";
+            employed.sayName();
+            Console.ReadLine();
+
+        }
+    }
+}
