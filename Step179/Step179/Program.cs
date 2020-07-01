18f9c892be3336276c162b937c11d766ecc8f7d3
NickW <nickwalker818323818@gmail.com>
2020-07-01 13:54:13 -0700

    Step 179

---
+++ Step179/Step179/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step179
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employed = new Employee();
+            employed.FirstName = "Sample ";
+            employed.LastName = "Student";
+            employed.sayName();
+            Console.ReadLine();
+        }
+    }
+}
