1bad11ef12b3008aa8773ed538605b41cb359133
NickW <nickwalker818323818@gmail.com>
2020-07-02 10:20:24 -0700

    2tnnr

---
+++ Generics/Generics/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee<string> employee = new Employee<string>();
+            Employee<string> employeeNumber = new Employee<string>();
+
+            employee.Things = "peach";
+            employeeNumber.Things = 484;
+
+            Console.WriteLine(employee.Things);
+            Console.WriteLine(employeeNumber.Things);
+
+
+
+        }
+    }
+}
