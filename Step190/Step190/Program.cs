2b574f7eb6975051075f49d124656c9c3fe21fb0
NickW <nickwalker818323818@gmail.com>
2020-07-02 11:38:44 -0700

    Step 190

--- Step190/Step190/Program.cs
+++ Step190/Step190/Program.cs
@@ -13,15 +13,17 @@ namespace Step190
             Employee<string> employee = new Employee<string>();
             Employee<int> employeeNumber = new Employee<int>();
             employeeNumber.Thing = new List<int>();
+            employee.Thing = new List<string>();
+
 
             employeeNumber.Thing.Add(474);
             employee.Thing.Add("Nick");
 
-            foreach (int num in employeeNumber)
+            foreach (int num in employeeNumber.Thing)
             {
                 Console.WriteLine(num);
             }
-            foreach (string employ in employee)
+            foreach (string employ in employee.Thing)
             {
                 Console.WriteLine(employ);
             }
