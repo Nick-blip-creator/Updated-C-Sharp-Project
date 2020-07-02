cd678c5350f27cdcf7070d56b87f0afbc2aacd91
NickW <nickwalker818323818@gmail.com>
2020-07-02 11:27:14 -0700

    Assistance

---
+++ Step190/Step190/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step190
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee<string> employee = new Employee<string>();
+            Employee<int> employeeNumber = new Employee<int>();
+            employeeNumber.Thing = new List<int>();
+
+            employeeNumber.Thing.Add(474);
+            employee.Thing.Add("Nick");
+
+            foreach (int num in employeeNumber)
+            {
+                Console.WriteLine(num);
+            }
+            foreach (string employ in employee)
+            {
+                Console.WriteLine(employ);
+            }
+
+
+            Console.Read();
+        }
+
+        
+    }
+}
