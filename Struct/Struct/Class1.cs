ce3c2db0898f3605d42d2e56cc1cc7f041ef41ce
NickW <nickwalker818323818@gmail.com>
2020-07-02 11:05:48 -0700

    Step 200

---
+++ Struct/Struct/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Structs
+{
+    class Class1
+    {
+        public struct Number
+        {
+            public decimal Numbers { get; set; }
+        }
+    }
+}
---
+++ Struct/Struct/Program.cs
@@ -0,0 +1,21 @@
+﻿using Structs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Structs.Class1;
+
+namespace Struct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number number = new Number();
+            number.Numbers = 748.5m;
+            Console.WriteLine(number.Numbers);
+            Console.Read();
+        }
+    }
+}
