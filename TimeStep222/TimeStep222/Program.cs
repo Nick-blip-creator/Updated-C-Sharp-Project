552058aebe0b932635595b612173504272d31393
NickW <nickwalker818323818@gmail.com>
2020-07-03 16:42:21 -0700

    Step222 DateTime

---
+++ TimeStep222/TimeStep222/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeStep222
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime now = DateTime.Now;
+            Console.WriteLine(now);
+            int userInput = Convert.ToInt32(Console.ReadLine());
+            DateTime date2 = now.AddHours(userInput);
+            Console.WriteLine(date2);
+            
+            Console.Read();
+        }
+    }
+}
