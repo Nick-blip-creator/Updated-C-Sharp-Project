125ba23ab7e858a0a293aaf5eb5cffdea1ccc323
NickW <nickwalker818323818@gmail.com>
2020-07-03 15:13:18 -0700

    Step 219

---
+++ TextFileStep219/TextFileStep219/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace TextFileStep219
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter in a number:");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+            string convertedToStringUserInput = userInput.ToString();
+            File.WriteAllText(@"C:\logs.txt", convertedToStringUserInput);
+            string log = File.ReadAllText(@"C:\logs.txt");
+            Console.WriteLine(log);
+            Console.Read();
+        }
+    }
+}
