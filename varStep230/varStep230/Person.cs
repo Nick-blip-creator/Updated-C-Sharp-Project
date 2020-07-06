b6a39d2bc9c5ddb9255d6c503d16ff1027f1b57c
NickW <nickwalker818323818@gmail.com>
2020-07-06 10:48:43 -0700

    Step 230

---
+++ varStep230/varStep230/Person.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace varStep230
+{
+    public class Person
+    {
+        
+
+        public  string firstName {get; set;}
+        public string lastName { get; set; }
+        public IEnumerable<Person> Children { get;}
+
+
+        public Person()
+        {
+            Children = new List<Person>();
+        }
+        public Person(string firstName)
+            : this()
+        {
+            this.firstName = firstName;
+        }
+        public Person(string firstName,string lastName)
+            : this(firstName)
+        {
+            this.lastName = lastName;
+        }
+
+        
+    }
+}
---
+++ varStep230/varStep230/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace varStep230
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string nickName = "Nick";
+            Console.WriteLine(nickName);
+            var newName = new Person("Nick");
+            Console.WriteLine(newName);
+            Console.Read();
+        }
+    }
+}
