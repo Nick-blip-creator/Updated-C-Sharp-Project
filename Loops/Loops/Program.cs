e4d0a0ce639a767e4c6387932f46d920fda4c808
NickW <nickwalker818323818@gmail.com>
2020-07-08 13:48:48 -0700

    Step 121

---
+++ Loops/Loops/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Using a While loop:");
+
+            bool isGuessed = false;
+
+            while (!isGuessed)
+            {
+                Console.WriteLine("What is your favorite fruit?");
+                string fruit = Console.ReadLine();
+                switch (fruit)
+                {
+                    case "Apples":
+                        Console.WriteLine("Apples are good, but not correct. Try again.");
+                        break;
+                    case "Bananas":
+                        Console.WriteLine("Unfortunately, that is wrong. Try again.");
+                        break;
+                    case "Watermelon":
+                        Console.WriteLine("Nice choice!");
+                        isGuessed = true;
+                        break;
+                    default:
+                        Console.WriteLine(fruit + " is not correct.");
+                        break;
+                }
+            }
+            Console.WriteLine("Using a Do While loop:");
+            Console.WriteLine("What is your favorite fruit?");
+            string fruity = Console.ReadLine();
+
+            bool correctFruit = fruity == "Watermelon";
+
+            do
+            {
+                switch (fruity)
+                {
+                    case "Apples":
+                        Console.WriteLine("Sorry, Apples is not the right answer.");
+                        Console.WriteLine("What is your favorite fruit?");
+                        fruity = Console.ReadLine();
+                        break;
+                    case "Bananas":
+                        Console.WriteLine("That is not correct.");
+                        Console.WriteLine("What is your favorite fruit?");
+                        fruity = Console.ReadLine();
+                        break;
+                    case "Watermelon":
+                        Console.WriteLine("You are correct!");
+                        correctFruit = true;
+                        break;
+                    default:
+                        Console.WriteLine(fruity + " is not correct. Try again.");
+                        Console.WriteLine("What is your favorite fruit?");
+                        fruity = Console.ReadLine();
+                        break;
+
+                }
+            }
+            while (!correctFruit);
+            Console.Read();
+        }
+    }
+}
---
+++ TechAcadMVC/TechAcadMVC/Controllers/HomeController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TechAcadMVC.Models;
+
+namespace TechAcadMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Contact Page - The Tech Academy.";
+
+            return View();
+        }
+        public ActionResult Instructors()
+        {
+            List<Instructor> instructors = new List<Instructor>
+            {
+                new Instructor
+                {
+                    Id = 1,
+                    FirstName = "James",
+                    LastName = "Marcello"
+                },
+                new Instructor
+                {
+                    Id = 2,
+                    FirstName = "Brett",
+                    LastName = "Chandler"
+                },
+                new Instructor
+                {
+                    Id = 3,
+                    FirstName = "Adam",
+                    LastName = "Smithosian"
+                }
+            };
+            return View(instructors);
+        }
+        public ActionResult Instructor(int id)
+        {
+            ViewBag.Id = id;
+            Instructor dayTimeInstructor = new Instructor
+
+            {
+                Id = 1,
+                FirstName = "Nick",
+                LastName = "Walker"
+            };
+
+            return View(dayTimeInstructor);
+        }
+    }
+}
