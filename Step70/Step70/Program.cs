7222f5a987aa1e326f7af45df0e6a2cc5946ed49
NickW <nickwalker818323818@gmail.com>
2020-07-09 17:16:35 -0700

    Step 70 CORRCT

--- Step70/Step70/Program.cs
+++ Step70/Step70/Program.cs
@@ -15,15 +15,18 @@ namespace Step70
             Console.WriteLine("What course are you on?");
             string currentCourse = Console.ReadLine();
             Console.WriteLine("What page number?");
-            string pageNumber = Console.ReadLine();
+            int pageNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(pageNumber);
             Console.WriteLine("Do you need help with anything? Please answer 'true' or 'false'.");
-            string needsHelp = Console.ReadLine();
+            bool needsHelp = Convert.ToBoolean(Console.ReadLine());
+            Console.WriteLine(needsHelp);
             Console.WriteLine("Were there any positive experiences you'd like to share? Please give specifics.");
             string positiveExperience = Console.ReadLine();
             Console.WriteLine("Is there any other feedback you'd like to provide? Please be specific.");
             string otherFeedback = Console.ReadLine();
             Console.WriteLine("How many hours did you study today?");
-            string hoursStudied = Console.ReadLine();
+            int hoursStudied = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(hoursStudied);
             Console.WriteLine("Thank you for your answers. An Instructor will respond to this shortly. Have a great day!");
             Console.Read();
         }
